836cc0e11e32339be05414b12fee9c62a081f96c
Anastasia <anastasia.gribovskaia@gmail.com>
2022-06-25 18:27:24 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function toReadable (number) {
-  
+    const numWords = require('num-words');
+    let numbers_word = numWords(number);
+    return numbers_word.split('and ').join('');
 }
