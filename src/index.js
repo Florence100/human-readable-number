bdf214b4f4538f52486d8549f4e745c3b55aa8de
Anastasia <anastasia.gribovskaia@gmail.com>
2022-06-30 01:25:04 +0300

    feat: Human Readable Number

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 module.exports = function toReadable (number){
-    const numWords = require('num-words');
-    let numbers_word = numWords(number);
-    return numbers_word.split('and ').join('');
+    let arr_one = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen',];
+    let arr_two = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety',];
+    let newString = '';
+    if (number === 0) {
+        return 'zero';
+    }      
+    if (String(number).length === 3) {
+        newString = newString + arr_one[Number(String(number)[0])] + ' hundred';
+        number = Math.round((number/100 - Math.floor(number/100)) * 100);
+    }
+    if (String(number).length === 2) {
+        if (Math.floor(number/10) === 1) {
+            newString = newString + " " + arr_one[number];
+        } else {
+            newString = newString + " " + arr_two[Math.floor(number/10)-2];
+            number = Math.round((number/10 - Math.floor(number/10)) * 10);
+        }
+    }
+    if (String(number).length === 1) {
+        newString = newString + " " + arr_one[Number(String(number)[0])];
+    }
+    return newString.trim();
 };
